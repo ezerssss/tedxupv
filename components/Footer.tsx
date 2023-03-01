c2ab8c056e6c9abc830a95ad5daf29ffa772d3f4
Ezra Magbanua <magbanuaezra@gmail.com>
2023-03-01 18:47:05 +0800

    [Adjustment][Ezra] duranne bug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,21 +6,29 @@ import twitter from '../public/logos/twitter.png';
 
 function Footer() {
     return (
-        <footer className="bg-[url('../public/images/stars.jpeg')] bg-cover bg-center px-24 py-28 text-center md:flex items-center justify-between">
-            <p className="text-white font-bold text-5xl drop-shadow">
+        <footer className="bg-[url('../public/images/stars.jpeg')] bg-cover bg-center md:px-24 py-20 md:py-28 text-center md:flex items-center justify-between">
+            <p className="text-white font-bold text-4xl md:text-5xl drop-shadow">
                 CONTACT US
             </p>
-            <div className="flex gap-7 items-center justify-center mt-10 md:mt-0">
+            <div className="flex gap-3 md:gap-7 items-center justify-center mt-10 md:mt-0">
                 <button>
-                    <Image alt="email" className="h-16 w-auto" src={email} />
+                    <Image
+                        alt="email"
+                        className="h-14 md:h-16 w-auto"
+                        src={email}
+                    />
                 </button>
                 <button>
-                    <Image alt="facebook" className="h-16 w-auto" src={fb} />
+                    <Image
+                        alt="facebook"
+                        className="h-14 md:h-16 w-auto"
+                        src={fb}
+                    />
                 </button>
                 <button>
                     <Image
                         alt="twitter"
-                        className="h-16 w-auto"
+                        className="h-14 md:h-16 w-auto"
                         src={twitter}
                     />
                 </button>
